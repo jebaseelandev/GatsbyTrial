5a1a0087462bb4b23e06eabaca67bb6c22592157
jebaseelandev <58473370+jebaseelandev@users.noreply.github.com>
2022-02-04 12:51:23 +0530

    layout changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -83,6 +83,7 @@ const links = [
   {
     text: "Tutorial",
     url: "https://www.gatsbyjs.com/docs/tutorial/",
+    image:"http://books.google.com/books/content?id=gsK9jwEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
     description:
       "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
     color: "#E95800",
@@ -90,6 +91,7 @@ const links = [
   {
     text: "How to Guides",
     url: "https://www.gatsbyjs.com/docs/how-to/",
+    image:"http://books.google.com/books/content?id=gsK9jwEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
     description:
       "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
     color: "#1099A8",
@@ -97,38 +99,43 @@ const links = [
   {
     text: "Reference Guides",
     url: "https://www.gatsbyjs.com/docs/reference/",
+    image:"http://books.google.com/books/content?id=gsK9jwEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api",
     description:
       "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
     color: "#BC027F",
   },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
+  // {
+  //   text: "Conceptual Guides",
+  //   url: "https://www.gatsbyjs.com/docs/conceptual/",
+  //   description:
+  //     "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
+  //   color: "#0D96F2",
+  // },
+  // {
+  //   text: "Plugin Library",
+  //   url: "https://www.gatsbyjs.com/plugins",
+  //   description:
+  //     "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
+  //   color: "#8EB814",
+  // },
+  // {
+  //   text: "Build and Host",
+  //   url: "https://www.gatsbyjs.com/cloud",
+  //   badge: true,
+  //   description:
+  //     "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
+  //   color: "#663399",
+  // },
 ]
 
 // markup
 const IndexPage = () => {
-  return ( 
-    'Hello Welcome' 
+  return (   
+    <div>      
+      {links.map(function(d, idx){
+          return (<><li key={idx}>{d.url}</li><img src={d.image} alt="A dog smiling in a party hat" /></>)
+        })}
+    </div>      
   )
 }
 
