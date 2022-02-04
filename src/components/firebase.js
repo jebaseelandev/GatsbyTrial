8ea41deefd35a3aa382c401c741c9d82fe9b06ae
jebaseelandev <58473370+jebaseelandev@users.noreply.github.com>
2022-02-04 12:50:06 +0530

    updates

---
+++ src/components/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC1AoQ-up3SDeeyzfukbmBWVRUI7y4TXng",
+  authDomain: "gatsbytest-3d1e5.firebaseapp.com",
+  projectId: "gatsbytest-3d1e5",
+  storageBucket: "gatsbytest-3d1e5.appspot.com",
+  messagingSenderId: "148852232652",
+  appId: "1:148852232652:web:ca28e07ae51de4d47d8d69"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
